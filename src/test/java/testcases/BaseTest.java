0a47b3e210a84bc3141d1145d431451ea7c2d1e2
CamiB <camy_nyk@yahoo.com>
2022-12-06 12:41:28 +0200

    Second commit-log4j added

--- src/test/java/testcases/BaseTest.java
+++ src/test/java/testcases/BaseTest.java
@@ -1,4 +1,8 @@
+package testcases;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeMethod;
@@ -12,13 +16,17 @@ public class BaseTest {
     WebDriver driver;
     HomePage homePage;
     BookPage bookPage;
+    Logger logger;
 
     @BeforeMethod
     public void setup() {
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
+
+        logger= LogManager.getLogger(BaseTest.class.getName());
         driver.manage().window().maximize();
         driver.get("https://demoqa.com/books");
+
 
         homePage = new HomePage(driver);
         bookPage = new BookPage(driver);
@@ -26,13 +34,16 @@ public class BaseTest {
 
     @Test
     public void combinedTest() throws InterruptedException {
+
         homePage.clickLogin()
                 .fullLogin()
                 .wait(5)
                 .clickFirstBook();
         bookPage.addBookToCollection();
+        logger.info("Added book");
         Thread.sleep(5000);
         bookPage.clickOkBtnOnAlertBox();
+        logger.info("Accepted alert");
         bookPage.logOut();
     }
 }
